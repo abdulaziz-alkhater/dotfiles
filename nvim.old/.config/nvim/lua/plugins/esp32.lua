4ae3ba11d38924ab132384178ec354a63b509c8d
Abdulaziz Al Khater <aks.alkhater@pm.me>
2026-09-02 15:45:57 +0300

    removed the linux stuff

--- nvim.old/.config/nvim/lua/plugins/esp32.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "Aietes/esp32.nvim",
-  opts = {
-    build_dir = "build",  -- CMake build directory for compile_commands.json
-  },
-  dependencies = {
-    "folke/snacks.nvim",  -- Required for terminal and picker interfaces
-  },
-}
